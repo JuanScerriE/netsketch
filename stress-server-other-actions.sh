1998fe5b99673e0e5a1c0660dce4ac3f574a8129
Juan Scerri <juan.scerri.js@gmail.com>
2024-06-22 14:23:16 +0200

    update report

---
+++ stress-server-other-actions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# set -x
+
+max=$(($1 - 1))
+
+echo "stress-server.sh: starting the test clients..."
+for i in `seq 0 $max`
+do
+    build/src/netsketch_test_client --username juan$i --iterations $2 --interval $3 --expected-responses $(($1*$2)) --other-actions &
+
+    pids[$i]=$!
+done
+
+echo "stress-server.sh: waiting for all test clients to finish..."
+for pid in ${pids[*]}
+do
+    wait $pid
+done
